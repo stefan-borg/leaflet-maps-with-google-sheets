30df59f84f59e51f49222fa0b306a135d71c3a13
stefan-borg <52224238+stefan-borg@users.noreply.github.com>
2019-06-26 10:39:19 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1QsABXa4uFfGHPMijNRx78rqBWGDbTOt3Glb1rafEgDQ/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1t4yXlwB8Mhfg652Togix7XUptr2BGr5fw-sa2IfXZeY/edit#gid=0';
